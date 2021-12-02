3442a585f56fe03bfe54bf3c9fb06cb5facac380
Calvin Swomley <88415921+calvinswomley@users.noreply.github.com>
2021-12-01 19:39:28 -0700

    Schema setup

--- schema.sql
+++ schema.sql
@@ -2,5 +2,21 @@ DROP DATABASE IF EXISTS subscription_db;
 
 CREATE DATABASE subscription_db;
 
-id, user, email, password
+USE subscription_db;
 
+CREATE TABLE user (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+user VARCHAR(30) NOT NULL,
+email NOT NULL,
+password NOT NULL
+);
+
+CREATE TABLE subscription (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+user_id INT,
+subscription_name VARCHAR(60) NOT NULL,
+spend INT,
+FOREIGN KEY (user_id)
+REFERENCES user(id)
+ON DELETE SET NULL
+);
