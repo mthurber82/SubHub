72aebf4777e99ad361bed73f7630d4329dd998ee
msp4msps <msp4msps@tminus365.com>
2021-12-04 10:50:25 -0700

    add login functionality

--- models/index.js
+++ models/index.js
@@ -1,4 +1,12 @@
 const User = require("./User");
 const Subscription = require("./Subscription");
 
+User.hasMany(Subscription, {
+  foreignKey: "user_id",
+});
+
+Subscription.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
 module.exports = { User, Subscription };
