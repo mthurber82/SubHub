e4ed1e2e1983b5fed5d4e1fd79dd918323d7f81f
msp4msps <msp4msps@tminus365.com>
2021-12-04 13:16:17 -0700

    add Subscription

--- public/js/subscriptions.js
+++ public/js/subscriptions.js
@@ -1,1 +1,22 @@
-const 
+const createHandler = async (event) => {
+  event.preventDefault();
+  const subscription_name = document.querySelector("#subname").value;
+  const spend = document.querySelector("#spendmonthly").value;
+
+  if (subscription_name && spend) {
+    const response = await fetch("api/subscription", {
+      method: "POST",
+      body: JSON.stringify({ subscription_name, spend }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert("Failed to create subscription");
+    }
+  }
+};
+
+document.querySelector(".AddSub").addEventListener("submit", createHandler);
