1069559647a705c38efe2d03c21d87d34d194e4e
msp4msps <msp4msps@tminus365.com>
2021-12-06 18:46:56 -0700

    create Chart

--- Controllers/api/subscriptionRoutes.js
+++ Controllers/api/subscriptionRoutes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { User, Subscription } = require("../../models");
+const withAuth = require("../../utils/auth");
 
 //Create New Subscription
 
@@ -17,3 +18,24 @@ router.post("/", async (req, res) => {
 });
 
 module.exports = router;
+
+//Get all subscriptions for user
+
+router.get("/", withAuth, async (req, res) => {
+  try {
+    const getSubs = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: Subscription }],
+    });
+    const subscriptions = getSubs.get({ plain: true });
+    res.status(200).json({
+      status: "Success",
+      data: subscriptions,
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "Fail",
+      message: "Could not get users subscriptions",
+    });
+  }
+});
