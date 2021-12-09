34d4b4475cb9bc85ab173c5b0163fde9395c2b03
msp4msps <msp4msps@tminus365.com>
2021-12-09 12:30:23 -0700

    add new chart

--- public/js/createBarChart.js
+++ public/js/createBarChart.js
@@ -1,14 +1,17 @@
-const months = ["January", "February", "March"];
-
+let apps = [];
+let usage = [];
 const bars = {
-  labels: months,
+  labels: apps,
   datasets: [
     {
-      label: "My First Dataset",
-      data: [65, 59, 80],
+      label: "Hrs/Week",
+      data: [2, 24, 30, 1],
       backgroundColor: [
         "rgba(255, 99, 132, 0.2)",
-        "rgba(255, 159, 64, 0.2)",
+        "rgb(54, 162, 235, 0.2)",
+        "rgba(255, 205, 86, 0.2)",
+        "rgba(255, 99, 132, 0.2)",
+        "rgb(54, 162, 235, 0.2)",
         "rgba(255, 205, 86, 0.2)",
       ],
       borderColor: [
@@ -31,4 +34,29 @@ const configuration = {
     },
   },
 };
+
+const getSpend = async () => {
+  const response = await fetch("/api/subscription");
+  const subData = await response.json();
+  console.log(subData);
+  if (response.ok) {
+    const subs = subData.data.subscriptions;
+    console.log(subs);
+    subs.map((sub) => apps.push(sub.subscription_name));
+    subs.map((sub) => usage.push(sub.usage));
+    console.log(usage);
+  }
+};
+
 var barChart = new Chart(document.getElementById("barChart"), configuration);
+console.log(barChart);
+function addD(chart, labels, usage) {
+  chart.data.datasets[0].data.push(usage);
+  chart.update();
+}
+
+async function preAddition() {
+  const usage = await getSpend();
+  addD(barChart, labels, usage);
+}
+preAddition();
