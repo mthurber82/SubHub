fc2ce0e26cd1d35f1993a1754c71697d5dfa8083
Carson <carsoncolgateccu@gmail.com>
2021-12-11 13:35:55 -0700

    update folder names

--- models/subscription.js
+++ models/subscription.js
@@ -1,6 +1,6 @@
 const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require("../config0/connection");
+const sequelize = require("../config/connection");
 
 class Subscription extends Model {}
 
